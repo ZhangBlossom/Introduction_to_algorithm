a5880880c29f01b31009677203a2b6e4369590f7
zhangjinbiao <460219753@qq.com>
2023-05-18 17:02:09 +0800

    write some string topic

---
+++ src/main/java/com/leetcode/learn/string/easy/WordNums.java
@@ -0,0 +1,20 @@
+package com.leetcode.learn.string.easy;
+
+/**
+ * @author: 张锦标
+ * @date: 2023/5/17 10:54
+ * WordNums类
+ */
+public class WordNums {
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+}
