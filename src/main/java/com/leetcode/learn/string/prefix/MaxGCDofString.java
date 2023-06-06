94d66c2dfaa0a6ef27f07d052e3ff2d4c7ffb4df
zhangjinbiao <460219753@qq.com>
2023-06-06 21:55:34 +0800

    finish cglig and jdk proxy

---
+++ src/main/java/com/leetcode/learn/string/prefix/MaxGCDofString.java
@@ -0,0 +1,34 @@
+package com.leetcode.learn.string.prefix;
+
+/**
+ * @author: 张锦标
+ * @date: 2023/6/6 21:45
+ * MaxGCDofString类
+ */
+public class MaxGCDofString {
+    public static String gcdOfStrings(String str1, String str2) {
+        int m = str1.length(), n = str2.length();
+        for (int i = Math.min(m, n); i >= 1; i--) {
+            if (m % i == 0 && n % i == 0) {
+                String X = str1.substring(0, i);
+                if (check(X, str1) && check(X, str2)) {
+                    return X;
+                }
+            }
+        }
+        return "";
+    }
+
+    private static boolean check(String x, String str1) {
+        int len = str1.length() / x.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= len; i++) {
+            sb.append(x);
+        }
+        return sb.toString().equals(str1);
+    }
+
+    public static void main(String[] args) {
+        gcdOfStrings("ABCABC","ABC");
+    }
+}
---
+++ src/main/java/com/leetcode/learn/tanxing/middle/LiheZuiTian.java
@@ -0,0 +1,36 @@
+package com.leetcode.learn.tanxing.middle;
+
+import java.util.Arrays;
+
+public class LiheZuiTian{
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+
+        // 二分模板·其三（开区间写法）https://www.bilibili.com/video/BV1AP41137w7/
+        int left = 0, right = (price[price.length - 1] - price[0]) / (k - 1) + 1;
+        while (left + 1 < right) { // 开区间不为空
+            // 循环不变量：
+            // f(left) >= k
+            // f(right) < k
+            int mid = left + (right - left) / 2;
+            if (f(price, mid) >= k) {
+                left = mid; // 下一轮二分 (mid, right)
+            } else {
+                right = mid; // 下一轮二分 (left, mid)
+            }
+        }
+        return left;
+    }
+
+    private int f(int[] price, int d) {
+        int cnt = 1, pre = price[0];
+        for (int p : price) {
+            if (p >= pre + d) {
+                cnt++;
+                pre = p;
+            }
+        }
+        return cnt;
+    }
+}
+
