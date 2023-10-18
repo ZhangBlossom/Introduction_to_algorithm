d1dcc1af6a0bf164107986b9ff08558545ee9be3
zhangbiaobiao <460219753@qq.com>
2023-10-18 19:46:25 +0800

    排序算法

--- src/main/java/datastructure/bit/OddTimesNum2.java
+++ src/main/java/datastructure/bit/OddTimesNum2.java
@@ -9,29 +9,48 @@ package datastructure.bit;
  * 奇数次的数据
  */
 public class OddTimesNum2 {
-    public static void printOddTimesNumb2(int[]arr){
+    public static void printOddTimesNumb2(int[] arr) {
         int eor = 0;
-        for(int i = 0;i<arr.length;i++){
-            eor^=arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            eor ^= arr[i];
         }
         //eor = a^b;
         //eor != 0
         //eor中必须有一个位上位1，因为如果都为0就是0了
         //得到最右边上面的哪一个为1的位
-        int rightOne = eor & ~(eor+1);
+        int rightOne = eor & ~(eor + 1);
         int onlyOne = 0; //eor'
         //此时对于a和b，由于他们至少在一个位上不同，那么我们就针对这个位
         //因为其他数据出现偶数次，所以让他们参与异或运算不会影响结果
         //此时我们只针对这个位为1的数据，而a和b中只能有一个数据在这个位上为1，因为如果
         //他们都为1，那么就得到0了，不合理，所以这里我们选择eor上面最右边的那个为1的位进行与运算
         //就能得到最后的onlyOne就是a或者b
-        for(int cur:arr){
-            if (((cur&rightOne)==1)){
-                onlyOne^=cur; //假设得到a
+        for (int cur : arr) {
+            if (((cur & rightOne) == 1)) {
+                onlyOne ^= cur; //假设得到a
             }
         }
         //得到a或者b之后，我们再一次异或，就是对方了。
         //那么这里就是b
-        System.out.println(onlyOne+"  "+(eor^onlyOne));
+        System.out.println(onlyOne + "  " + (eor ^ onlyOne));
     }
+
+    public int[] singleNumber(int[] nums) {
+        int xorsum = 0;
+        for (int num : nums) {
+            xorsum ^= num;
+        }
+        // 防止溢出
+        int lsb = (xorsum == Integer.MIN_VALUE ? xorsum : xorsum & (-xorsum));
+        int type1 = 0, type2 = 0;
+        for (int num : nums) {
+            if ((num & lsb) != 0) {
+                type1 ^= num;
+            } else {
+                type2 ^= num;
+            }
+        }
+        return new int[]{type1, type2};
+    }
+
 }
---
+++ src/main/java/leetcode/list/A_IntersectListNode.java
@@ -0,0 +1,94 @@
+package leetcode.list;
+
+/**
+ * @author: 张锦标
+ * @date: 2023/10/17 21:03
+ * A_IntersectListNode类
+ */
+public class A_IntersectListNode {
+    /**
+     * 解决链表有无环的问题，有两种方式，一种是用set集合去存储节点
+     * 那么只要存储节点放入节点失败的时候，就说明出现了重复的值，此时就是环
+     * 第二种方式使用快慢指针，比较难。
+     * 大概思路是，快指针每次跳两步，慢指针每次一步。
+     * 在有限次数的机会里，如果有环，快慢指针最终一定会相遇，如果没有，快指针最终指向null。
+     * 并且，在他们相遇之后，让快指针回到头节点并且每次都只走一步，慢指针也是一样，之后
+     * 他们两个再次相遇的时候，就说明当前节点就是那个产生环的节点了。
+     *
+     * @param head
+     * @return
+     */
+    public static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node n1 = head.next; //slow
+        Node n2 = head.next.next; //fast
+        while (n1 != n2) {
+            if (n2.next == null || n2.next.next == null) {
+                //说明无环直接返回
+                return null;
+            }
+            //否则每次都快慢指针走法
+            n2 = n2.next.next;
+            n1 = n1.next;
+            //只要n1！=n2说明还没有相遇
+            while (n1 != n2) {
+                n1 = n1.next;
+                n2 = n2.next;
+            }
+        }
+        return n1;//最终相遇的节点就是环
+    }
+
+    /**
+     * 对于两个相交的链表 链表末尾位置的节点一定相等
+     * 所以可以遍历一下两个链表得到长度
+     * 比如第一个100，第二个80
+     * 那么可以让第一个先走20步，那么只要这两个链表最终相交，
+     * 两个节点一定可以相遇
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    //如果两个链表都无环 返回第一个相交节点 如果不相交 直接返回null
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        //两个节点都走到末尾后不相等 说明无公共节点
+        if (cur1 != cur2) {
+            return null;
+        }
+        cur1 = n > 0 ? head1 : head2; //谁长，谁的头变成cur1
+        cur2 = cur1 == head1 ? head2 : head1; //谁短，谁的头变成cur2
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+    //不存在一个有环一个无环
+    //两个有环链表
+    public ListNode intersectListNode(ListNode head) {
+
+        return null;
+    }
+}
